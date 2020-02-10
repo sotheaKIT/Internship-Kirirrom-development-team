ac369e0167fb9734cdfa4150fbd43d4a52c63c3d
Thaung Monyodam <you@example.com>
2020-02-10 14:34:05 +0700

    ddd

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
+<style src="./styles/app.css"></style>
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomePage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './view/HomePage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/view/HomePage.vue
@@ -0,0 +1,21 @@
+
+<style src="../styles/home.css"></style>
+<template>
+  <div class="hello">
+    <Navigator/>
+  </div>
+</template>
+
+<script>
+import Navigator from '../components/navigator/navigator.vue'
+
+
+export default {
+  name: 'HomePage',
+  components: {
+    Navigator
+  }
+}
+
+</script>
+
