e5e987a8fceabab27c3d5948f381d4538d4d5043
sayching <chingsay888@gmail.com>
2020-02-12 20:35:16 +0700

    footer

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,8 @@
+<style src="./footer.css"></style>
+<template src="./footer.html"></template>
+
+<script>
+    export default {
+        name: 'Footer'
+    }
+</script>
--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -3,17 +3,21 @@
 <template>
   <div class="hello">
     <Navigator/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navigator from '../components/navigator/navigator.vue'
+import Footer from '../components/footer/footer.vue'
 
 
 export default {
   name: 'HomePage',
   components: {
-    Navigator
+    Navigator,
+    // eslint-disable-next-line vue/no-unused-components
+    Footer
   }
 }
 
