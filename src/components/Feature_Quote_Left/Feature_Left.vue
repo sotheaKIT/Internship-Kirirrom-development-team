3d50f4e2d91f0ef86706fc0249787c3e3b9171ee
sanysak <sanysaklucky@gmail.com>
2020-02-17 17:34:43 +0700

    fixed: duplicated name

--- src/components/Feature_Quote_Left/Feature_Left.vue
+++
@@ -1,8 +0,0 @@
-<style src="./Feature_Left.css"></style>
-<template src="./Feature_Left.html"></template>
-
-<script>
-export default {
-  name : 'Feature_Left'
-}
-</script>
--- src/components/Feature_Quote_Left_Top/feature_quoate_left.vue
+++
@@ -1,8 +0,0 @@
-<template src="./feature_quote_left.html"></template>
-<style src="./feature_quote_left.css"></style>
-<script>
-export default {
-    name: 'Feature_quote_left'
-}
-</script>
-
